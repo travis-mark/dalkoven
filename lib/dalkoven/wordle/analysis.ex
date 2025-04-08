74a02d00c2812fe32737e35dd229de15024e4c42
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2025-04-08 15:19:39 -0400

    First pass

--- lib/dalkoven/wordle/analysis.ex
+++ lib/dalkoven/wordle/analysis.ex
@@ -1,24 +1,8 @@
 defmodule Dalkoven.Wordle.Analysis do
-  import Ecto.Query
-
-  @doc """
-  Get a list of Wordle solutions older than date.
-
-  Used to keep statistical analysis or guessing from seeing today's answer.
-  """
-  def solutions_older_than(date_string) do
-    query =
-      from p in Dalkoven.Wordle.Puzzle,
-        where: p.print_date < ^date_string,
-        select: p.solution
-
-    query |> Dalkoven.Repo.all()
-  end
-
   @doc """
   Count the frequency of each letter for analysis
 
-  iex(8)> ["unite", "these"] |> Analysis.count_character_frequencies
+  iex> ["unite", "these"] |> Analysis.count_character_frequencies
   %{101 => 3, 104 => 1, 105 => 1, 110 => 1, 115 => 1, 116 => 2, 117 => 1}
   """
   def count_character_frequencies(words) do
@@ -32,9 +16,21 @@ defmodule Dalkoven.Wordle.Analysis do
     end)
   end
 
+  @doc """
+  Score a word, based on a frequency count.
+  """
   def word_score(frequencies, word) do
     word
     |> String.to_charlist()
     |> Enum.reduce(0, fn char, sum -> sum + frequencies[char] end)
+  end
+
+  def guess_next_word(date) do
+    allowed = Dalkoven.Wordle.Puzzle.get_wordle_answers()
+    solutions = Dalkoven.Wordle.Puzzle.solutions_older_than(date)
+    freq = count_character_frequencies(solutions)
+
+    (allowed -- solutions)
+    |> Enum.max(&(word_score(freq, &1) > word_score(freq, &2)))
   end
 end
--- lib/dalkoven/wordle/puzzle.ex
+++ lib/dalkoven/wordle/puzzle.ex
@@ -1,6 +1,7 @@
 defmodule Dalkoven.Wordle.Puzzle do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query
 
   @primary_key {:id, :integer, autogenerate: false}
   schema "wordle_puzzles" do
@@ -16,5 +17,29 @@ defmodule Dalkoven.Wordle.Puzzle do
     |> cast(attrs, [:days_since_launch, :editor, :id, :print_date, :solution])
     |> unique_constraint(:id)
     |> validate_required([:days_since_launch, :editor, :id, :print_date, :solution])
+  end
+
+  @doc """
+  Get a list of Wordle solutions older than date.
+
+  Used to keep statistical analysis or guessing from seeing today's answer.
+  """
+  def solutions_older_than(date_string) do
+    query =
+      from p in Dalkoven.Wordle.Puzzle,
+        where: p.print_date < ^date_string,
+        select: p.solution
+
+    query |> Dalkoven.Repo.all()
+  end
+
+  def put_wordle_answers(data) do
+    binary = :erlang.term_to_binary(data)
+    File.write!("priv/bin/wordle_answers.bin", binary)
+  end
+
+  def get_wordle_answers do
+    binary = File.read!("priv/bin/wordle_answers.bin")
+    :erlang.binary_to_term(binary)
   end
 end
